98d32e65acf3441e0ab4cb8897c63e3c31ae6ce1
Santosh Kumar Nayak <santonayak@deloitte.com>
2022-03-22 09:46:37 +0530

    class members order adjusted as per csv column

--- src/main/java/com/huedge/NetfllixBingeWebAPI/model/Netflix.java
+++ src/main/java/com/huedge/NetfllixBingeWebAPI/model/Netflix.java
@@ -3,43 +3,38 @@ package com.huedge.NetfllixBingeWebAPI.model;
 public class Netflix {
     private String showId;
     private String type;
-    private String country;
-    private String date_added;
     private String name;
     private String director;
     private String cast;
+    private String country;
+    private String date_added;
     private String release_year;
     private String season_code;
     private String season_count;
     private String category;
     private String description;
-    public Netflix(String showId, String type, String country, String date_added, String name, String director, String cast, String release_year, String season_code, String season_count, String category, String description) {
+
+    public Netflix(String showId, String type, String name, String director, String cast, String country, String date_added, String release_year, String season_code, String season_count, String category, String description) {
         this.showId = showId;
         this.type = type;
-        this.country = country;
-        this.date_added = date_added;
         this.name = name;
         this.director = director;
         this.cast = cast;
+        this.country = country;
+        this.date_added = date_added;
         this.release_year = release_year;
         this.season_code = season_code;
         this.season_count = season_count;
         this.category = category;
         this.description = description;
-    }    public String getShowId() {
+    }
+
+    public String getShowId() {
         return showId;
     }
 
     public String getType() {
         return type;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getDate_added() {
-        return date_added;
     }
 
     public String getName() {
@@ -52,6 +47,14 @@ public class Netflix {
 
     public String getCast() {
         return cast;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDate_added() {
+        return date_added;
     }
 
     public String getRelease_year() {
@@ -73,5 +76,4 @@ public class Netflix {
     public String getDescription() {
         return description;
     }
-
 }
